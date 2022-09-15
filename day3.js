b10ef7dd5eaed3fe936cb1eff2d4e2bef21be08d
harshita.sharma <harshita.sharma@asksid.ai>
2022-09-15 23:00:27 +0530

    day 3

---
+++ day3.js
@@ -0,0 +1,224 @@
+// CONTROL FLOW
+// conditional statements, loops 
+
+// if block , if-else , if-else ladder
+
+
+// Q : If number is odd then print ODD. 
+
+
+// // if(a%2 != 0){
+// //     console.log("Value a " , a, " is odd.");
+// // }
+
+// (a%2 != 0) && (console.log("Value a " , a, " is odd."))
+
+
+// logical &&
+
+
+// let a = 13;
+
+
+// if(a%2 == 0){
+//     console.log("EVEN");
+// }else{
+//     console.log("ODD");
+// }
+
+// Ternary operator 
+
+// (a%2 == 0) ? console.log("Even") : console.log("odd");
+
+// console.log("END");
+
+// let gender="MALE"
+
+// if (gender ==="male" || gender == "MALE") {
+//     console.log("MALE");
+// }else if (gender ==="female" || gender == "FEMALE"){
+//     console.log("FEMALE");
+// }else {
+//     console.log("OTHERS");
+// }
+
+//Week Plan
+// const num = 5;
+
+// switch(num){
+//     case 0:
+//         console.log("Monday");
+//         break;
+//     case 1:
+//         console.log("Tuesday");
+//         break;
+//     case 2:
+//         console.log("Wednesday");
+//         break;
+//     case 3:
+//         console.log("Thursday");
+//         break;
+//     case 4:
+//         console.log("Friday");
+//         break;
+//     case 5:
+//         console.log("Saturday");
+//         break;
+//     case 6:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Number not valid");
+// }
+
+
+
+// Grading Plan
+// 100 - 80 : "A";
+// 80 - 60 : "B";
+// 60 - 40 : "C";
+// 40 - 10 : "Fail"
+
+
+
+const marks = 40;
+switch (marks) {
+    case 100 > marks && marks > 80:
+        console.log("A");
+        break;
+    case 80 >= marks && marks > 60:
+        console.log("B");
+        break;
+    case 60 > marks && marks >= 40:
+        console.log("C");
+        break;
+    case 40 > marks && marks > 0:
+        console.log("D");
+        break;
+    default:
+        console.log("Provide valid marks");
+        break;
+}
+
+
+
+
+// let marks= 50;
+// switch(marks>80? "A": 1){
+//     case "A":
+//         console.log("A");
+//         break;
+//     case 1:
+//     switch(marks>60? "B": 2){
+//         case "B":
+//             console.log("B");
+//             break;
+//             case 2:
+//             switch(marks>40? "C": "Fail"){
+//                 case "C":
+//                     console.log("C");
+//                     break;
+//                     case "Fail":
+//                         console.log("Fail");
+//             }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function add (a){
+//     let temp = [...a,1,2,4,4]
+//     return temp
+// }
+
+// let arr = [1,2,3];
+// const value = add(arr)
+// console.log(value);
+
+
+// function handleSubmit (){
+//     const name = document.getElementById("name");
+//     const element = document.createElement("h1");
+//     element.innerHTML = name.value;
+//     const divTag = document.getElementById("content");
+//     divTag.appendChild(element);
+
+//     console.log(element);
+//     // alert(name.value);
+// }
+
+
+{/* <h1>John Doe</h1> */}
+
+
+// 1> Create tag 
+// 2> Add data to tag fetched from user 
+// 3> add this tag on the page
+
+
+
+// 11am - 1pm  => Saturday Please JOIN 
+
+
+
+lOOPS , objects 
+
